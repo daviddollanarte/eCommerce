4f538e693d8412a9e689d4cb0777110b00ad24aa
daviddollanarte <87336994+daviddollanarte@users.noreply.github.com>
2021-08-19 07:12:18 -0300

    Redirects index=>login

--- js/login.js
+++ js/login.js
@@ -2,5 +2,5 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
-
+    window.location.replace("https://daviddollanarte.github.io/eCommerce/login");
 });
