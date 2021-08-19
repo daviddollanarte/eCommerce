0ab2f4e50792b012cb6708184fce1ee733100e4e
daviddollanarte <87336994+daviddollanarte@users.noreply.github.com>
2021-08-19 06:56:49 -0300

    Creates Login HTML & Modifies Products HTML

--- js/products.js
+++ js/products.js
@@ -2,5 +2,13 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
+    var lista=document.getElementById("lista");
+    fetch (PRODUCTS_URL)
+        .then(resp=>(resp.json()) 
+        .then(data =>{
+            lista.innerHTML=`<ul></ul>`
+            for (i in data[i]){
 
-});
+            }
+        }))
+})
