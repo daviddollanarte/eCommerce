b8b920bff05296d0ec1eda8c06294d861cb96111
daviddollanarte <87336994+daviddollanarte@users.noreply.github.com>
2021-08-19 11:42:10 -0300

    Modifies form method

--- js/products.js
+++ js/products.js
@@ -3,13 +3,18 @@ document.addEventListener("DOMContentLoaded", function (e) {
     fetch ("https://japdevdep.github.io/ecommerce-api/product/all.json")
         .then(resp=>(resp.json()) 
         .then(data =>{
-            lista.innerHTML=`<ul></ul>`
+            lista.innerHTML=`
+            <h2 class="pb-5 text-center p-4">Productos</h2>
+            <p class="lead">Verás aquí todos los productos de esta categoría.</p>
+            <ul></ul>`
             for (let i = 0; i < data.length; i++) {
                 lista.innerHTML+=
                 `<div class="container pb-5 my-3 list-group">
-                <li>`+data[i].name+`</li> 
-                <li>`+data[i].currency+` `+data[i].cost+`</li>
-                <li>`+data[i].description+`</li>
+                <li>`+data[i].name+`
+                <hr>
+                `+data[i].currency+` `+data[i].cost+`
+                <hr>
+                `+data[i].description+`</li>
                 <hr>
                 </div>
                 `
