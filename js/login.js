415a453604d5ee8bc51f51dfc080983d525b40df
daviddollanarte <87336994+daviddollanarte@users.noreply.github.com>
2021-09-04 08:25:03 -0300

    Fixes another logout problem

--- js/login.js
+++ js/login.js
@@ -1,8 +1,6 @@
 function login(){
  if(!localStorage.getItem('logedin')) {
-  console.log(localStorage.getItem("logedin"))
     window.location.replace("login.html");
-    localStorage.setItem("logedin","true");
   } 
 }
 
--- js/username.js
+++ js/username.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(e){
     document.getElementById("loginpg").addEventListener("submit", function(){
+        localStorage.setItem("logedin","true")
         localStorage.setItem("user", document.getElementById("user").value)
     })
 })
