e4604e7d42a782357d5fe4ee2e18d7fe6ead18b0
daviddollanarte <87336994+daviddollanarte@users.noreply.github.com>
2021-09-07 18:26:01 -0300

    Changes login script

--- js/login.js
+++ js/login.js
@@ -1,7 +1,10 @@
+var googlelogin=false;
 function login(){
- if(!localStorage.getItem('logedin')) {
+ if((!localStorage.getItem('logedin')) && !(googlelogin)) {
     window.location.replace("login.html");
   } 
 }
-
+function login2(googleUser){
+  googlelogin=(googleUser.getId()!="");
+}
 document.addEventListener("DOMContentLoaded", login());
