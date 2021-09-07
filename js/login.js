4a0c829bc4ac5bbad3e266021330971087213478
daviddollanarte <87336994+daviddollanarte@users.noreply.github.com>
2021-09-07 18:28:42 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -5,6 +5,7 @@ function login(){
   } 
 }
 function login2(googleUser){
+  console.log(JSON.stringify(googleUser.getBasicProfile()))
   googlelogin=(googleUser.getId()!="");
 }
 document.addEventListener("DOMContentLoaded", login());
