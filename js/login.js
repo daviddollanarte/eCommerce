60d9aae3a6dca2bf75a245b4e0f6142a1a8d8512
daviddollanarte <87336994+daviddollanarte@users.noreply.github.com>
2021-09-08 09:24:13 -0300

    Google Sign in update

--- js/login.js
+++ js/login.js
@@ -1,12 +1,7 @@
 var googlelogin = false;
 function login(googleUser) {
-  console.log(JSON.stringify(googleUser.getBasicProfile()))
-  googlelogin = (googleUser.getId() != "");
-  if ((!localStorage.getItem('logedin')) && !(googlelogin)) {
+  if (!localStorage.getItem('logedin')) { 
     window.location.replace("login.html");
   }
-}
-function login2(googleUser) {
-
 }
 document.addEventListener("DOMContentLoaded", login());
