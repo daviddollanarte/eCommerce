28b1d7452bf518925a338031d4684c0aac58ee87
daviddollanarte <87336994+daviddollanarte@users.noreply.github.com>
2021-10-01 15:16:47 -0300

    Adds related products\Modifies dropdown menu

--- js/product-info.js
+++ js/product-info.js
@@ -1,12 +1,13 @@
 var productInfoArray = [],
     productComments = [],
     pag = document.getElementById("products-info"),
-    commentBox=document.getElementById("commentBox"),
-    newCommentBox=document.getElementById("newCommentBox"),
-    starRating=0;
+    commentBox = document.getElementById("commentBox"),
+    newCommentBox = document.getElementById("newCommentBox"),
+    starRating = 0;
 
 function showProductInfo() {
     pag.innerHTML = "";
+    //Agrega al inicio de la página los datos del artículo, el nombre, una descripción, el costo, la cantidad de vendidos y un carrusel con las imágenes del producto
     pag.innerHTML += `<div class="container">
     <div class="pb-5 p-4">
     <h1 style="font-weight: bold">`+ productInfoArray.name + `</h1><hr>`
@@ -47,54 +48,60 @@ function showProductInfo() {
     </div>
     <a type="button" class="btn btn-light btn-lg btn-block" id="commentsBtn">Mostrar comentarios</a>
   </div>`
-}
+}//El último div agrega un botón para acceder a los comentarios
 function showDate(dateString) {
-    let dateSplit = dateString.split(" "),
-        dateDay = dateSplit[0].split("-"),
-        dateHour = dateSplit[1].split(":"),
-        dateFinal = "";
-    dateFinal = dateDay[2] + "/" + dateDay[1] + "/" + dateDay[0] + " " + dateHour[0] + ":" + dateHour[1];
-    return dateFinal;
+    let dateSplit = dateString.split(" "),//Divide el string entre fecha y hora
+        dateDay = dateSplit[0].split("-"),//Divide la fecha en dia, mes y año
+        dateHour = dateSplit[1].split(":");//Divide la horas y minutos
+        
+    return dateDay[2] + "/" + dateDay[1] + "/" + dateDay[0] + " " + dateHour[0] + ":" + dateHour[1];//Carga la fecha en el formato deseado
 }
 
-function showName(nameString) {
-    var newName = "";
-    let nameArr = nameString.split("_");
-    for (i = 0; i < nameArr.length; i++) {
-        let word = nameArr[i]
-        newName += word.charAt(0).toUpperCase() + word.slice(1) + " ";
+function showName(nameString) {//nameString viene en formato nombre_apellido y deseamos mostrarlos en un formato más agradable.
+    if (nameString != localStorage.getItem("user")) {//Formatea el nombre si el comentario no lo esté haciendo el... 
+        //usuario que está logeado actualmente.
+        var newName = "";
+        let nameArr = nameString.split("_");
+        for (i = 0; i < nameArr.length; i++) {
+            let word = nameArr[i]
+            newName += word.charAt(0).toUpperCase() + word.slice(1) + " ";//Cambia a mayúscula el primer caracter de cada palabra del nombre.
+        }
+        return newName;
     }
-    return newName;
+    else {
+        return nameString;//Si el comentario que se quiere mostrar lo hizo el usuario que está logueado actualmente el...
+        //formato del nombre puede no tener un patrón reconocible para poder formatearlo por lo tanto se lo muestra como está
+    }
 }
 function showStars(rating) {
     let content = "";
-    for (i = 0; i < 5; i++) {
+    for (i = 0; i < 5; i++) {//Muestra cinco estrellas.
         if (i < rating) {
-            content += `<span class="fa fa-star checked"></span>`
+            content += `<span class="fa fa-star checked"></span>`//Toma el puntaje que le dió el usuario y muestra esa cantidad en amarillo.
         } else {
-            content += `<span class="fa fa-star"></span>`
+            content += `<span class="fa fa-star"></span>`//El resto las muestra en negro.
 
         }
     }
     return content;
 }
-function boxColor(rating){
-    if (rating<5){
-        if ( rating<3){
+function boxColor(rating) {//Dependiendo de la puntuación el borde del comentario lo muestra en rojo, amarillo o verde
+    if (rating < 5) {
+        if (rating < 3) {
             return "border border-danger";
         }
         return "border border-warning";
     }
-  return "border border-success";
+    return "border border-success";
 }
 function showComments() {
     commentBox.innerHTML += `
     <h3>Comentarios:</h3>
     <hr>
     `
-    for (let i = 0; i < productComments.length; i++) {
+    for (let i = 0; i < productComments.length; i++) {//Muestra el cada comentario en el arreglo con el formato deseado
         commentBox.innerHTML += `
-        <div class="container rounded bg-light `+boxColor(productComments[i].score)+`">
+        <div class="container rounded bg-light `+ boxColor(productComments[i].score) + `">
         <h4>`+ showName(productComments[i].user) + `:</h4>
         <p class="text-muted">`+ showDate(productComments[i].dateTime) + `</p>
         <p>`+ productComments[i].description + `</p>
@@ -106,8 +113,8 @@ function showComments() {
 
     }
 }
-function showNewCommentBox(){
-    newCommentBox.innerHTML=`
+function showNewCommentBox() {//Muestra el cuadro de texto para escribir el comentario, las cinco estrellas y el botón de enviar comentario.
+    newCommentBox.innerHTML = `
     <div class="container">
         <div class="row">
             <div class="col">
@@ -131,51 +138,93 @@ function showNewCommentBox(){
 
     `
 }
-function saveStarRating(rating){
-    //add flag variable
+function saveStarRating(rating) {//Cuando se hace click en una estrella se guarda el puntaje correspondiente en la variable starRating.
     starRating = rating;
 };
-function changeStarCheck(index){
-    for (i=0; i<index; i++){
-        let currentStar="star"+(i+1),
-            starElement=document.getElementById(currentStar);
+function changeStarCheck(index) {//Cambia las estrellas de negro a amarillo cuando pasamos el mouse encima de ellas
+    for (i = 0; i < index; i++) {
+        let currentStar = "star" + (i + 1),
+            starElement = document.getElementById(currentStar);
         starElement.classList.add("checked");
     }
 };
-function resetCheck(){
-    for (i=1; i<5; i++){
-        let currentStar="star"+(i+1),
-        starElement=document.getElementById(currentStar);
-        starElement.classList.remove("checked");
+function resetCheck() {
+    if (starRating != 0) {//Si se ha dado click a alguna de las estrellas las estrellas anteriores quedan en amarillo y las siguientes vuelven a negro.
+        for (i = starRating; i < 5; i++) {
+            let currentStar = "star" + (i + 1),
+                starElement = document.getElementById(currentStar);
+            starElement.classList.remove("checked");
+        }
+    } else {
+        for (i = 1; i < 5; i++) {//Si no se ha dado click a ninguna estrella entonces todas menos la primera estrella quedan negras al quitar el mouse.
+            let currentStar = "star" + (i + 1),
+                starElement = document.getElementById(currentStar);
+            starElement.classList.remove("checked");
+        }
     }
 }
 
-function saveNewComment(){
-    var newComment=document.getElementById("newComment"),
-        sendBtn=document.getElementById("sendBtn");
-        sendBtn.addEventListener("click", function(){
-            if (!(sessionStorage.getItem("commentsOn?"))){
-                let newCommentArray={
-                    score: starRating,
-                    description: newComment.value,
-                    user: localStorage.getItem("user"),
-                    dateTime: new Date(),
-                };
-                newComment.value="";
-                console.log(newCommentArray)
-
-                /*agregar el comentario directo al array con comentarios */
+function saveNewComment() {//Agrega el comentario que se realizó a la lista de comentarios mostrados
+    var newComment = document.getElementById("newComment"),//Accedo al cuadro de texto.
+        sendBtn = document.getElementById("sendBtn");//Accedo al botón de enviar comentario
+    sendBtn.addEventListener("click", function () {
+        if ((starRating == 0) || (newComment.value == "")) {
+            alert("Completa todos los campos antes de comentar!")//Si el texto está vacío o no se seleccionó puntaje con las estrellas se manda una alerta que pide que complete todos los campos.
+        } else {
+            let newDate = new Date(),//Carga en la variable newDate la fecha y hora de en que se realizó el comentario.
+                formatedDate = "";
+            formatedDate = newDate.getFullYear() + "-" + (newDate.getMonth() + 1) + "-" + newDate.getDate() + " " + newDate.getHours() + ":" + newDate.getMinutes();//Guarda la fecha en el mismo formato que la fecha que viene en el JSON.
+            var newCommentArray = {//Crea un objeto isomorfo a los comentarios presentes en el JSON.
+                score: starRating,
+                description: newComment.value,
+                user: localStorage.getItem("user"),
+                dateTime: formatedDate,
+            };
+            newComment.value = "";//Vacía el cuadro de texto luego que se realizó el comentario
+            starRating=0;//Resetea el valor de la calificación
+            resetCheck();//Las estrellas vuelven a estar desmarcadas
+            productComments.push(newCommentArray);//Agrega el nuevo comentario al arreglo que contiene los anteriores
+            if (sessionStorage.getItem("commentsOn?")) {//Si ya se habían desplegado los comentarios, los borra y los vuelve a desplegar para que aparezca el que acabamos de mandar.
+                commentBox.innerHTML = "";
+                showComments();
             }
-            else {
-                let newCommentArray={
-                    score: starRating,
-                    description: newComment.value,
-                    user: localStorage.getItem("user"),
-                    dateTime: new Date(),
-                };
-                newComment.value="";
-                console.log(newCommentArray)            }
-        })
+
+        }
+    })
+}
+function addRelatedProducts() {
+    fetch(PRODUCTS_URL)//Va a buscar el listado de productos completo.
+        .then(resp => (resp.json())
+            .then(function (data) {
+                let relatedProducts = document.getElementById("relatedProducts");//Agrego la información de los productos relacionados, foto y nombre en un link.
+                relatedProducts.innerHTML = `
+            <div class="container">
+            <h3>Productos relacionados:</h3>
+            <br><br>
+                <div class="row">
+                    <div class="col-2">
+                    </div>
+                    <div class="col">
+                        <a href="products.html">
+                            <img src="`+data[productInfoArray.relatedProducts[0]].imgSrc+//Accedo a la ruta de la imagen...
+                            //..que está en el arreglo que contiene el listado de productos utilizando el índice de artículo...
+                            //..que viene en el atributo relatedProducts del objeto con la información del producto
+                            `" width="200px">
+                            <h4 style="color:black">`+data[productInfoArray.relatedProducts[0]].name+
+                            //Mismo procedimiento que con la imagen
+                            `</h4>         
+                        </a>
+                    </div>
+                    <div class="col">
+                        <a href="products.html">
+                        <img src="`+data[productInfoArray.relatedProducts[1]].imgSrc+`" width="200px">
+                        <h4 style="color:black">`+data[productInfoArray.relatedProducts[1]].name+`</h4>         
+                        </a>
+                    </div>
+                </div>
+            </div>
+            `        }))
+
 }
 document.addEventListener("DOMContentLoaded", function (e) {
     fetch(PRODUCT_INFO_URL)
@@ -183,28 +232,30 @@ document.addEventListener("DOMContentLoaded", function (e) {
             .then(function (data) {
                 productInfoArray = data;
                 showProductInfo()
-                let commentsBtn=document.getElementById("commentsBtn");
+                let commentsBtn = document.getElementById("commentsBtn");
                 sessionStorage.removeItem("commentsOn?");
                 showNewCommentBox();
-                saveNewComment();
-                console.log(new Date())
+                addRelatedProducts();
                 commentsBtn.addEventListener("click", function () {
-                    if (!(sessionStorage.getItem("commentsOn?"))) {
-                        sessionStorage.setItem("commentsOn?",true)
+                    if (!(sessionStorage.getItem("commentsOn?"))) {//Si los comentarios están ocultos los muestra
+                        sessionStorage.setItem("commentsOn?", true)
                         fetch(PRODUCT_INFO_COMMENTS_URL)
                             .then(resp => (resp.json())
                                 .then(function (data) {
-                                    productComments = data;
+                                    for (let i = 0; i < data.length; i++) {
+                                        productComments.push(data[i]);
+                                    }
                                     commentsBtn.innerHTML = "Ocultar comentarios"
                                     showComments()
                                 }))
-                    } else {
-                        console.log(sessionStorage.getItem("commentsOn?"));
+                    } else {//Si los comentarios están activados el botón va a mostrar "Ocultar comentarios" y al hacer click los va a ocultar.
                         sessionStorage.removeItem("commentsOn?");
-                        commentBox.innerHTML="";
-                        commentsBtn.innerHTML="Mostrar comentarios";
+                        commentBox.innerHTML = "";
+                        commentsBtn.innerHTML = "Mostrar comentarios";
+                        productComments = [];
                     }
                 })
+                saveNewComment();
 
             }))
 });
--- js/userdata.js
+++ js/userdata.js
@@ -1,14 +1,14 @@
 function userdata() {
     document.getElementById("UserName").innerHTML = localStorage.getItem("user")
 }
-userdata();
+userdata();//Carga el nombre de usuario de local storage en el botón desplegable
 document.getElementById("logout").addEventListener("click", function () {
+    localStorage.clear()//Elimina los datos de local storage que tiene la info del usuario
     var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
+    auth2.signOut().then(function () {//Cierra sesión si está ingresado con google
         console.log('User signed out.');
         localStorage.clear();
         login()
     }
     )
-    localStorage.clear()
 })
